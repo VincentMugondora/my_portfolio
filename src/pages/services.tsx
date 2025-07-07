237cf4c831c0c0812616fd226fec55e70377de76
vincentmugondora <vinmugondora@gmail.com>
2025-07-07 08:54:13 +0200

    Refactor TopSkills component: remove skill percentage display to streamline visual presentation and enhance user experience.

---
+++ src/pages/services.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ExperiencedServices from '../components/ExperiencedServices';
+
+const ServicesPage: React.FC = () => {
+  return (
+    <main className="min-h-screen bg-[#111111]">
+      <ExperiencedServices />
+    </main>
+  );
+};
+
+export default ServicesPage; 
